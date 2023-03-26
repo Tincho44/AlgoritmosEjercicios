1b1fa4a2b1827a6a2ad7b3eb30ce658b1b382c30
Martin <107601706+Tincho44@users.noreply.github.com>
2023-03-26 15:08:57 -0300

    Create Ejercicio_9.java

---
+++ UT1_PD5/Ejercicio_9.java
@@ -0,0 +1,17 @@
+//a)
+
+String hannah = "Did Hannah see bees? Hannah did.";
+System.out.println(hannah.length());
+
+//32 es el resultado
+
+//b)
+
+System.out.println(hannah.charAt(12));
+
+// resultado = e
+
+//c)
+
+hannah.indexOf("b"); //Esto me dice en que posicion esta
+System.out.println(hannah.charAt(hannah.indexOf("b"))); // Esto me da como retorno b
