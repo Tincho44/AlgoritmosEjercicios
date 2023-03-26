380107261230aab2bb25cfe08f33eede93c71e1b
Martin <107601706+Tincho44@users.noreply.github.com>
2023-03-26 04:01:57 -0300

    Create Ejercicio_5.java

---
+++ UT1_PD5/Ejercicio_5.java
@@ -0,0 +1,18 @@
+// Parte 1:
+
+/*
+run:
+3 digits before decimal point.
+2 digits after decimal point.
+BUILD SUCCESSFUL (total time: 0 seconds)
+*/
+
+// Parte 2: 
+
+/*
+
+Lo que hace la funcion es pasar el double a un string, luego de pasarlo, busca donde esta el "." , cuenta en que posicion esta e imprime
+en donde los caracteres antes del punto. Para contar cuantos caracteres tiene luego del punto, invierte el string y cuenta de nuevo e imprime cuantos ahi despues.
+
+*/
+
