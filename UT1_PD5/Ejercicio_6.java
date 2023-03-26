4a71047554e24bf909d4165f717373210459b2e4
Martin <107601706+Tincho44@users.noreply.github.com>
2023-03-26 04:10:04 -0300

    Create Ejercicio_6.java

---
+++ UT1_PD5/Ejercicio_6.java
@@ -0,0 +1,26 @@
+/*
+
+● substring -> Te toma parte de un string, puede ser el inicio o el final. Hay que pasarle un rango. 
+
+● split -> Le pasas como parametro algo, y cada vez que encuentra eso en un string, crea una cadena de strings limitados por el parametro pasado.
+
+● subSequence -> Le pasas como parametros el inicio y el fin del string y te retorna los valores comprendidos en ese rango.
+
+● trim -> 
+
+● toLowerCase -> 
+
+● toUpperCase -> 
+
+● indexOf –>
+
+● lastIndexOf –>
+
+● contains ->
+
+● replace -> 
+
+● replaceAll ->
+
+*/ 
+
