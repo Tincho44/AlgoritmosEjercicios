b0228d9d3492af6a17ef03fb4ccf2e23e9346944
Martin <107601706+Tincho44@users.noreply.github.com>
2023-03-26 03:33:25 -0300

    Create Ejercicio_2.java

---
+++ UT1_PD5/Ejercicio_2.java
@@ -0,0 +1,38 @@
+package palindrome;
+
+/**
+ *
+ * @author martinsilva
+ */
+public class Palindrome {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        String palindrome = "Dot. s,Aw .I was; Tod";
+        int len = palindrome.length();
+        char[] tempCharArray = new char[len];
+        char[] charArray = new char[len];
+        
+        // put original string in an 
+        // array of chars
+        for (int i = 0; i < len; i++) {
+            tempCharArray[i] = 
+                palindrome.charAt(i);
+        } 
+        
+        // reverse array of chars
+        for (int j = 0; j < len; j++) {
+            charArray[j] =
+                tempCharArray[len - 1 - j];
+        }
+        
+        String reversePalindrome =
+            new String(charArray);
+        System.out.println(reversePalindrome);
+        
+    }
+}
+
