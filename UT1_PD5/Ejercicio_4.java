d2b88018bc0b3b1cf203543fd782cff75c4f5edf
Martin <107601706+Tincho44@users.noreply.github.com>
2023-03-26 03:55:25 -0300

    Create Ejercicio_4.java

---
+++ UT1_PD5/Ejercicio_4.java
@@ -0,0 +1,33 @@
+package ejerciciosparte5;
+
+/**
+ *
+ * @author martinsilva
+ */
+public class EjerciciosParte5 {
+    public static void main(String[] args) {
+
+        // this program requires two 
+        // arguments on the command line 
+        if (args.length == 2) {
+            // convert strings to numbers
+            float a = (Float.valueOf(args[0])).floatValue(); 
+            float b = (Float.valueOf(args[1])).floatValue();
+
+            // do some arithmetic
+            System.out.println("a + b = " +
+                               (a + b));
+            System.out.println("a - b = " +
+                               (a - b));
+            System.out.println("a * b = " +
+                               (a * b));
+            System.out.println("a / b = " +
+                               (a / b));
+            System.out.println("a % b = " +
+                               (a % b));
+        } else {
+            System.out.println("This program " +
+                "requires two command-line arguments.");
+        }
+    }
+}
