c23340dd3bf7f9b039e73ec94931a78736e31cf6
Martin <107601706+Tincho44@users.noreply.github.com>
2023-03-26 17:26:22 -0300

    Create Ejercicio_1.java

---
+++ UT1_PD8/Ejercicio_1.java
@@ -0,0 +1,35 @@
+public class Marcapasos {
+    
+    
+    short presionSanguinea;
+    
+    short frecuenciaSanguinea;
+    
+    short azucarSangre;
+    
+    long maxFuerza; 
+    
+    float tiempoLatidos;
+    
+    double bateriaRestante;
+    
+    byte codigoFabricante; 
+    
+}
+
+
+/*
+
+Espacio cada tipo de dato primitivo:
+
+byte: 1 byte
+short: 2 bytes
+int: 4 bytes
+long: 8 bytes
+float: 4 bytes
+double: 8 bytes
+char: 2 bytes
+
+En este caso, solamente con los tipos de datos primitivos, ocupo 27 bytes.
+
+*/
